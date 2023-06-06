b8594772f42d1bebd192adae4438415880a927e4
Kaamouchi-Fadi <Kaamouchi.fadi@gmail.com>
2023-06-06 13:22:33 -0700

    Fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for digits in range(0, 10):
     for digit in range(digits + 1, 10):
         if digits == 8 and digit == 9:
-            print("{}{}".format(digits, digit)
+        print("{}{}".format(digits, digit)
         else:
-            print("{}{}".formta(digits, digit), end=", ")
+        print("{}{}".formta(digits, digit), end=", ")
