9cd12a188e6a000d67bd7a05aab47c8f6a6c6034
Kaamouchi-Fadi <Kaamouchi.fadi@gmail.com>
2023-06-06 12:59:30 -0700

    fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
     if chr(letter) !="e" and chr(letter) != "q":
-        print(f"{chr(letter)}", end="")
+        print("{}".format(chr(letter)), end="")
