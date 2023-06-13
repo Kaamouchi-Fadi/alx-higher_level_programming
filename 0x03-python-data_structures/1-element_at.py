10b55d863289747969793ced42959d5ccbe90586
Kaamouchi-Fadi <125515402+Kaamouchi-Fadi@users.noreply.github.com>
2023-06-13 23:07:50 +0100

    fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if (len(my_list) - 1) < idx < 0:
+    if idx < 0 or idx > len(my_list) - 1:
         return 'None'
     else:
         return my_list[idx]
