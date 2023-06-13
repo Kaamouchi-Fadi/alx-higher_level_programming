d5bf7f35e1424ab20c60092a492311a242720f4a
Kaamouchi-Fadi <125515402+Kaamouchi-Fadi@users.noreply.github.com>
2023-06-13 00:27:13 +0000

    Python - Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    copy = my_list.copy()
+    if len(my_list) - 1 < idx < 0:
+        return my_list.copy()
+    else:
+        copy[idx] = element
+        return copy
